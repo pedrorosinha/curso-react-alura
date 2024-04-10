0ac1c524d89bec0669fa3216136a946fc021524b
pedro felipe <pedrofelipinho@gmail.com>
2024-04-10 11:24:49 -0300

    Criando componente Formulario e aplicando na tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Button from './components/button';
+import Form from './components/form';
 
 function App() {
   return (
     <div className="App">
-      <Button/>
+      <Form/>
     </div>
   );
 }
