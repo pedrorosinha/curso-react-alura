db5ea2ad999da11cb6af1e5ba1bf423c7baebedf
pedro felipe <pedrofelipinho@gmail.com>
2024-04-10 14:42:37 -0300

    Colocando o componente Lista e aplicando na tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './components/form';
+import List from './components/list';
 
 function App() {
   return (
     <div className="App">
       <Form/>
+      <List/>
     </div>
   );
 }
