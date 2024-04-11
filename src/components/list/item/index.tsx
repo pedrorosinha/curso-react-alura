5ae9f966674bc5b4e0d01e0bceb80cf5b9b9e51a
pedro felipe <pedrofelipinho@gmail.com>
2024-04-11 14:56:53 -0300

    Removendo o console.log

--- src/components/list/item/index.tsx
+++ src/components/list/item/index.tsx
@@ -6,7 +6,6 @@ interface Props extends ITarefa {
 }
 
 export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
-    console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
     return (
         <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} onClick={() => selecionaTarefa({
             tarefa,
