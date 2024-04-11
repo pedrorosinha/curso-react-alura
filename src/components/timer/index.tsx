588eb8199ad105be301ae3741f2fd0dd9929b9a1
pedro felipe <pedrofelipinho@gmail.com>
2024-04-11 16:44:57 -0300

    Convertendo o tempo em segundos

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,7 +1,9 @@
+import { TempoParaSegundos } from "../../common/utils/date";
 import Button from "../button";
 import Clock from "./clock";
 import style from './Timer.module.scss';
 export default function Timer() {
+    console.log('conversão: ', TempoParaSegundos('01:01:01'))
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
