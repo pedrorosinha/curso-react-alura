59e4ed90f16dabb4a761ed279c93c6b432c2050c
pedro felipe <pedrofelipinho@gmail.com>
2024-04-11 17:55:02 -0300

    Mostrando o tempo no cronômetro

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -21,9 +21,8 @@ export default function Timer({ selecionado }: Props) {
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-            Tempo: {tempo}
             <div className={style.relogioWrapper}>
-                <Clock />
+                <Clock tempo={tempo} />
             </div>
             <Button>
                 Começar!
