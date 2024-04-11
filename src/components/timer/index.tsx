49ec7df49f5cf733fa021a43ba17adf1333cf315
pedro felipe <pedrofelipinho@gmail.com>
2024-04-11 09:40:50 -0300

    Criando e estilizando o cronômetro

---
+++ src/components/timer/index.tsx
@@ -0,0 +1,16 @@
+import Button from "../button";
+import Clock from "./clock";
+import style from './Timer.module.scss';
+export default function Timer() {
+    return (
+        <div className={style.cronometro}>
+            <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+            <div className={style.relogioWrapper}>
+                <Clock/>
+            </div>
+            <Button>
+                Começar!
+            </Button>
+        </div>
+    )
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Form from '../components/form';
 import List from '../components/list';
 import style from './App.module.scss';
+import Timer from '../components/timer';
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Form/>
       <List/>
+      <Timer/>
     </div>
   );
 }
