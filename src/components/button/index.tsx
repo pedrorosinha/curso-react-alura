545f54c230dccb857ceb61647267b5b13df8e022
pedro felipe <pedrofelipinho@gmail.com>
2024-04-12 10:48:39 -0300

    Refatorando o botão com interface

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import style from './Button.module.scss';
 
-interface ButtonProps {
-    children: React.ReactNode;
-    type?: "button" | "submit" | "reset" | undefined, onClick?: () => void
-  }
-  
-  class Button extends React.Component<ButtonProps> {
-    render(): React.ReactNode {
-      const {type = "button", onClick} = this.props;
-      return (
-        <button onClick={onClick} className={style.botao} type={type}>
-            {this.props.children}
-        </button>
-      );
-    }
-  }
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined,
+  onClick?: () => void, children?: React.ReactNode
+}
+
+function Button({ onClick, type, children }: Props) {
+  return (
+    <button onClick={onClick} type={type} className={style.botao}>
+      {children}
+    </button>
+  )
+}
+// interface ButtonProps {
+//     children: React.ReactNode;
+//     type?: "button" | "submit" | "reset" | undefined, onClick?: () => void
+//   }
+
+// class Button1 extends React.Component<ButtonProps> {
+//   render(): React.ReactNode {
+//     const {type = "button", onClick} = this.props;
+//     return (
+//       <button onClick={onClick} className={style.botao} type={type}>
+//           {this.props.children}
+//       </button>
+//     );
+//   }
+// }
 
 export default Button;
